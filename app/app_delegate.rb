9f279343537f6eba48f30994929b09a362797608
Mark Rickert <mjar81@gmail.com>
2013-12-07 11:51:40 -0500

    Use v0.0.3 syntax for message scheduling.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -15,9 +15,9 @@ class AppDelegate
 
   def applicationDidBecomeActive(application)
     messages = MotionTakeoff::Messages.new
-    messages.message launch:1, title:"Welcome to #{App.name}!", message:"#{App.name} is a fun way to share unique text artwork with your friends!\n\nTap a Textie to get started!"
-    messages.message launch:3, title:"Quick Tip:", message:"If you favorite a Textie, it adds it to the top of this screen!"
-    messages.message launch:5, title:"Crazy Text".kanjify, message:"Tap the " << "crazy".kanjify << " button to create your own crazy text and send to friends!"
+    messages.schedule launch:1, title:"Welcome to #{App.name}!", message:"#{App.name} is a fun way to share unique text artwork with your friends!\n\nTap a Textie to get started!"
+    messages.schedule launch:3, title:"Quick Tip:", message:"If you favorite a Textie, it adds it to the top of this screen!"
+    messages.schedule launch:5, title:"Crazy Text".kanjify, message:"Tap the " << "crazy".kanjify << " button to create your own crazy text and send to friends!"
     messages.takeoff
 
     MotionTakeoff::Reminders.reset
