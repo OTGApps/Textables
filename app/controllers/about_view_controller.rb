7cd312f289a1b59cd7009b4ce8f2737e15fc2ce9
Mark Rickert <mjar81@gmail.com>
2013-12-08 13:14:31 -0500

    Add about VC

---
+++ app/controllers/about_view_controller.rb
@@ -0,0 +1,9 @@
+class AboutViewController < Formotion::FormController
+
+  def viewDidLoad
+    super
+    self.title = "About"
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemStop, target:self, action:"dismissModalViewControllerAnimated:")
+  end
+
+end
