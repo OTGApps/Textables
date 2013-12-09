36c8b88aa9dcd581dfc53605054a059ad73bfeff
Mark Rickert <mjar81@gmail.com>
2013-12-09 01:15:28 -0500

    Make sure the about page always closes animated.

--- app/controllers/about_view_controller.rb
+++ app/controllers/about_view_controller.rb
@@ -2,9 +2,12 @@ class AboutViewController < Formotion::FormController
 
   def viewDidLoad
     super
-
     self.title = "About"
-    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemStop, target:self, action:"dismissModalViewControllerAnimated:")
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemStop, target:self, action:"close")
+  end
+
+  def close
+    dismissModalViewControllerAnimated(true)
   end
 
 end
