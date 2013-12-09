075f5744e3a77acd8a3525fb5633175dbbb98a2a
Mark Rickert <mjar81@gmail.com>
2013-12-09 00:16:19 -0500

    Prompt the user to share the app on the 30th launch.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
 
-  attr_accessor :window
+  attr_accessor :window, :view_controller
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     # 3rd Party integrations
@@ -19,8 +19,8 @@ class AppDelegate
       Harpy.sharedInstance.checkVersion
     end
 
-    @texties_vc = TextiesViewController.alloc.initWithCollectionViewLayout(UICollectionViewFlowLayout.new)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(@texties_vc)
+    self.view_controller = TextiesViewController.alloc.initWithCollectionViewLayout(UICollectionViewFlowLayout.new)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(self.view_controller)
 
     self.window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     self.window.rootViewController = nav_controller
@@ -37,6 +37,13 @@ class AppDelegate
     messages.schedule launch:1, title:"Welcome to #{App.name}!", message:"#{App.name} is a fun way to share unique text artwork with your friends!\n\nTap a Textie to get started!"
     messages.schedule launch:5, title:"Quick Tip:", message:"If you favorite a Textie, it adds it to the top of this screen!"
     messages.schedule launch:10, title:"Crazy Text".kanjify, message:"Tap the " << "crazy".kanjify << " button to create your own crazy text and send to friends!"
+    messages.schedule(
+      launch: 30,
+      title: "Have You Shared #{App.name} Yet?",
+      message: "You've been using #{App.name} a lot!\n\nWant to share the app with some friends? We'd love you forever!",
+      action: Proc.new{ App.delegate.view_controller.show_info(nil) },
+      :buttons => ["No Thanks", "OK"]
+    )
     messages.takeoff
 
     Takeoff::Reminders.reset
