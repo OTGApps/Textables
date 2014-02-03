cd2c812792ded42dddaae456fa1e7d65f36c4679
Mark Rickert <mjar81@gmail.com>
2014-02-03 14:14:21 -0500

    Fix Textable text on alerts.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -41,8 +41,8 @@ class AppDelegate
 
   def applicationDidBecomeActive(application)
     @messages = Takeoff::Messages.new
-    @messages.schedule launch:1, title:"Welcome to #{App.name}!", message:"#{App.name} is a fun way to share unique text artwork with your friends!\n\nTap a Textie to get started!"
-    @messages.schedule launch:5, title:"Quick Tip:", message:"If you favorite a Textie, it adds it to the top of this screen!"
+    @messages.schedule launch:1, title:"Welcome to #{App.name}!", message:"#{App.name} is a fun way to share unique text artwork with your friends!\n\nTap a Textable to get started!"
+    @messages.schedule launch:5, title:"Quick Tip:", message:"If you favorite a Textable, it adds it to the top of this screen!"
     @messages.schedule launch:10, title:"Crazy Text".kanjify, message:"Tap the " << "crazy".kanjify << " button to create your own crazy text and send to friends!"
     @messages.schedule(
       launch: 30,
@@ -62,13 +62,13 @@ class AppDelegate
     return if App::Persistence['show_notifications'] == false
 
     (1..3).to_a.each do |interval|
-      message = "You haven't sent a Textie in #{interval} week"
+      message = "You haven't sent a Textable in #{interval} week"
 
       case interval
       when 1
         message << ". I bet your friends could use a laugh!"
       when 2
-        message << "s. Try sending a Textie to a friend!"
+        message << "s. Try sending a Textable to a friend!"
       when 3
         message << "s. We'll stop bothering you. ¯\\_(ツ)_/¯"
       end
